c20a20dcf982b44495ff1a2ce1904292697002eb
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-07 10:40:51 -0300

    TERMINADO

--- ejemplo-app/App.js
+++ ejemplo-app/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button, ScrollView, SectionList, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native';
+import  appStyles  from './styles.js';
 
 const DATA = [
   {
@@ -37,101 +38,40 @@ const DATA2 = [
 ];
 
 export default function App() {
-  const [count, setCount] = useState(0); 
-  const handlePress = () => setCount (count+ 1); 
+  const [count, setCount] = useState(0);
+  const handlePress = () => setCount(count + 1);
   return (
-    <View style={styles.container}>
-      <ScrollView style={styles.scrollView}>
-        <Text style={styles.text}>¡Hola!</Text>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity onPress={() => console.log("¡Botón presionado!")} >
-          <Text>Presionar</Text>
-            </TouchableOpacity>
-            
+    <View style={appStyles.container}>
+      <ScrollView style={appStyles.scrollView}>
+        <Text style={appStyles.text}>¡Hola!</Text>
+        <View style={appStyles.buttonContainer}>
+          <TouchableOpacity onPress={() => console.log("¡Botón presionado!")}>
+            <Text>Presionar</Text>
+          </TouchableOpacity>
         </View>
-        <Text> {'\n'}{'\n'}</Text>
 
         {DATA.map(button => (
-          <View style={styles.buttonContainer2} key={button.id}>
-            <TouchableOpacity onPress= { () => console.log("HOLAA!")} >
-            <Text>{button.title} </Text>
-            </TouchableOpacity> 
-
-           
+          <View style={appStyles.buttonContainer2} key={button.id}>
+            <TouchableOpacity onPress={() => console.log("HOLAA!")}>
+              <Text>{button.title}</Text>
+            </TouchableOpacity>
           </View>
-          
         ))}
 
         <StatusBar style="auto" />
-        <Text> {'\n'}{'\n'}</Text>
-      <SectionList
-        sections={DATA2}
-        keyExtractor={(item, index) => item + index}
-        renderItem={({item}) => (
-          <View style={styles.item}>
-            
-            <Text style={styles.title}>{item}</Text>
-          </View>
-        )}
-        renderSectionHeader={({section: {title}}) => (
-          <Text style={styles.header}>{title}</Text>
-        )}
-      />
-       </ScrollView>
+        <Text> {'\n'}{'\n'} </Text>
 
+        {DATA2.map(section => (
+          <View key={section.title}>
+            <Text style={appStyles.header}>{section.title}</Text>
+            {section.data.map(item => (
+              <View style={appStyles.item} key={item}>
+                <Text style={appStyles.title}>{item}</Text>
+              </View>
+            ))}
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#e8175d',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'white',
-    fontSize: 42,
-    lineHeight: 84,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginTop: 100,
-  },
-  buttonContainer: {
-    backgroundColor: '#f58dff',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 50,
-    marginTop: 50,
-    alignItems: 'center',
-  },
-  item: {
-    marginTop: 20,
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-  },
-  buttonContainer2: {
-    backgroundColor: '#f07877',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-    marginTop: 20,
-    alignItems: 'center',
-  },
-  scrollView: {
-    marginTop: 20,
-  },
-  title: {
-    color: 'black',
-    fontSize: 20,
-  },
-  header: {
-    color: 'white',
-    fontSize: 24,
-    fontWeight: 'bold',
-    backgroundColor: '#f9c2ff',
-    padding: 10,
-  },
-});
---
+++ ejemplo-app/styles.js
@@ -0,0 +1,54 @@
+const appStyles = {
+  container: {
+    flex: 1,
+    backgroundColor: '#e8175d',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 100,
+  },
+  buttonContainer: {
+    backgroundColor: '#f58dff',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 50,
+    marginTop: 50,
+    alignItems: 'center',
+  },
+  item: {
+    marginTop: 20,
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+  },
+  buttonContainer2: {
+    backgroundColor: '#f07877',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  scrollView: {
+    marginTop: 20,
+  },
+  title: {
+    color: 'black',
+    fontSize: 20,
+  },
+  header: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+    backgroundColor: '#f9c2ff',
+    padding: 10,
+  },
+};
+
+export default appStyles;
